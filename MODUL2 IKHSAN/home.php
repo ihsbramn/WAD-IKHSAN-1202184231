70ec97a7c0fb0d97b90db9a52bc880bb3f301596
Ikhsan Abdurachman <abdurahmanihsan25@gmail.com>
2020-10-10 18:25:12 +0700

    commit

--- MODUL2 IKHSAN/home.php
+++ MODUL2 IKHSAN/home.php
@@ -7,8 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
     <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css"
-        integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
     <link rel="stylesheet" href="style.css">
     <title>EAD HOTEL</title>
 </head>
@@ -18,17 +17,16 @@
     <nav class="navbar navbar-expand-lg navbar-light bg-primary fixed-top">
         <div class="container">
             <a class="navbar-brand" href="#"></a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
-                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarNav">
                 <ul class="navbar-nav mx-auto">
                     <li class="nav-item">
-                        <a class="nav-link active" href="home.html">Home</a>
+                        <a class="nav-link active" href="home.php">Home</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="booking.html">Booking</a>
+                        <a class="nav-link" href="booking.php">Booking</a>
                 </ul>
             </div>
         </div>
@@ -46,8 +44,7 @@
         <div class="row">
             <div class="col-sm">
                 <div class="card shadow mb-5 bg-white rounded" style="width: 18rem;">
-                    <img src="/MODUL2 IKHSAN/img/standardroom.jpeg" class="card-img-top" alt="..." height="250"
-                        width="250">
+                    <img src="/MODUL2 IKHSAN/img/standardroom.jpeg" class="card-img-top" alt="..." height="250" width="250">
                     <div class="card-body">
                         <h5 class="card-title font-weight-bold text-center">Standard</h5>
                         <h5 class=" card-title text-center text-primary"> $ 90/Day</h5>
@@ -65,8 +62,7 @@
             </div>
             <div class="col-sm">
                 <div class="card shadow mb-5 bg-white rounded" style="width: 18rem;">
-                    <img src="/MODUL2 IKHSAN/img/superiorroom.jpg" class="card-img-top" alt="..." height="250"
-                        width="250">
+                    <img src="/MODUL2 IKHSAN/img/superiorroom.jpg" class="card-img-top" alt="..." height="250" width="250">
                     <div class="card-body">
                         <h5 class="card-title font-weight-bold text-center">Superior</h5>
                         <h5 class="card-title text-center text-primary">$ 150/Day</h5>
@@ -87,8 +83,7 @@
             </div>
             <div class="col-sm">
                 <div class="card shadow mb-5 bg-white rounded" style="width: 18rem;">
-                    <img src="/MODUL2 IKHSAN/img/luxuryroom.jpg" class="card-img-top" alt="..." height="250"
-                        width="250">
+                    <img src="/MODUL2 IKHSAN/img/luxuryroom.jpg" class="card-img-top" alt="..." height="250" width="250">
                     <div class="card-body">
                         <h5 class="card-title font-weight-bold text-center">Luxury</h5>
                         <h5 class="card-title text-center text-primary">$ 200/Day</h5>
@@ -124,14 +119,11 @@
 
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
-        integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous">
     </script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"
-        integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous">
     </script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"
-        integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous">
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous">
     </script>
 </body>
 
