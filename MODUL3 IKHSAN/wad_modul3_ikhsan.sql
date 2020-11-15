9eeb43f0f3b757ea72035980d7499a91ed647563
Ikhsan Abdurachman <abdurahmanihsan25@gmail.com>
2020-11-15 20:15:38 +0700

    commit

---
+++ MODUL3 IKHSAN/wad_modul3_ikhsan.sql
@@ -0,0 +1,74 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Nov 15, 2020 at 02:06 PM
+-- Server version: 10.4.14-MariaDB
+-- PHP Version: 7.4.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `modul3`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `event_table`
+--
+
+CREATE TABLE `event_table` (
+  `id` int(11) NOT NULL,
+  `nama` varchar(100) NOT NULL,
+  `deskripsi` text NOT NULL,
+  `gambar` varchar(100) NOT NULL,
+  `kategori` varchar(100) NOT NULL,
+  `tanggal` date NOT NULL,
+  `mulai` time NOT NULL,
+  `berakhir` time NOT NULL,
+  `tempat` varchar(100) NOT NULL,
+  `harga` bigint(20) NOT NULL,
+  `benefit` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `event_table`
+--
+
+INSERT INTO `event_table` (`id`, `nama`, `deskripsi`, `gambar`, `kategori`, `tanggal`, `mulai`, `berakhir`, `tempat`, `harga`, `benefit`) VALUES
+(23, 'Webinar Santuy Masbramn', 'Seminar Santai Bersama Masbramn\r\nSemangat ', '1550282723_gege7e9m_400x400.jpg', 'Online', '2020-12-02', '09:15:00', '11:16:00', 'Zoom', 1500000, 'Sertifikat,Souvenir');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `event_table`
+--
+ALTER TABLE `event_table`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `event_table`
+--
+ALTER TABLE `event_table`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
